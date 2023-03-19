bde7ae126e57bb0e84dd54b3d15290ec8f5c7095
bodybri <bryanhuang404@gmail.com>
2023-03-18 17:42:06 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Bryan Huang on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
